9cf9415583d0a4628f37e78a7bdb83afb5e37eec
Raphael Lüthy <raphael@famluethy.ch>
2021-03-21 15:16:14 +0100

    "init"

--- src/main/java/ch/raphaelluethy/Main.java
+++ src/main/java/ch/raphaelluethy/Main.java
@@ -1,4 +1,4 @@
-package com.raphaelluethy;
+package ch.raphaelluethy;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
